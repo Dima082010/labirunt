6afc338daf57f91ac99f4d7b5e476e3474da0356
Dima B <dmytrob2010@icloud.com>
2023-06-18 10:49:20 +0300

    class Game_sprite

--- mai.py
+++ mai.py
@@ -17,6 +17,20 @@ clock = pygame.time.Clock()
 fon = pygame.image.load(fila_path(r"images\turma.jpg"))
 fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
 
+class Game_sprite(pygame.sprite.Sprite):
+    def __init__(self, x, y, width, height, image_name):
+        self.rect = pygame.Rect(x, y, width, height)
+        self.image = pygame.image.load(fila_path(image_name))
+        self.image = pygame.transform.scale(self.image, (width, height))
+
+    def show(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+player = Game_sprite(5, 6, 50, 70, r'images\Luffy.png')
+enemy = Game_sprite(1100, 500, 50, 70, r'images\Mochi.webp')
+finish = Game_sprite(500, 300, 40, 60, r'images\frukt.png')
+
+
 lvl = 1
 game = True
 
@@ -27,7 +41,9 @@ while game == True:
 
     if lvl == 1:
         window.blit(fon, (0, 0))
-        
+        player.show()
+        enemy.show()
+        finish.show()
 
     clock.tick(FPS)
     pygame.display.update()

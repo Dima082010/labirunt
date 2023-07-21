43569ae4f63f43d406b3338444adbff921236a93
Dima B <dmytrob2010@icloud.com>
2023-07-21 17:21:14 +0300

    dddd

--- mai.py
+++ mai.py
@@ -11,11 +11,11 @@ WIN_WIDTH = 1200
 WIN_HEIGHT = 700
 FPS = 40
 LVL_1 = (18, 17, 17)
-LVL_1_CLICK = (15, 14, 14)
+LVL_1_CLICK = (38, 37, 37)
 LVL_2 = (64, 23, 4)
-LVL_2_CLICK = (61, 20, 1)
+LVL_2_CLICK = (84, 43, 24)
 LVL_3 = (46, 124, 7)
-LVL_3_CLICK = (32, 74, 4)
+LVL_3_CLICK = (66, 144, 27)
 TEXT = (255, 255, 255)
 EXIT = (18, 78, 7)
 
@@ -221,104 +221,137 @@ class Bulet(Game_sprite):
 
 enemys = pygame.sprite.Group()
 
-enemy = Enemy(600, 480, 50, 70, r'images\prison.png', 0, 600, 'left', 4)
-enemys.add(enemy)
-enemy2 = Enemy(150, 140, 50, 70, r'images\Crocodile.png', 150, 680, 'right', 5)
-enemys.add(enemy2)
-enemy3 = Shot(1150, 110, 50, 90, r'images\katakuri.png', 0, 200, 'up', 5, 50)
-enemys.add(enemy3)
-enemy4 = Enemy(1150, 630, 50, 70, r'images\leopard.png', 155, 1150, 'left', 7)
-enemys.add(enemy4)
 
 
-player = Player(5, 6, 50, 70, r'images\Luffy.png')
-frukt = Game_sprite(90, 140, 40, 60, r'images\frukt.png')
-frukt2 = Game_sprite(90, 240, 40, 60, r"images\Mochi.png")
 frukt3 = Game_sprite(90, 240, 40, 60, r"images\frukt2.png")
-bonus = Game_sprite(250, 250, 50, 70, r'images\bonus.png')
-key = Game_sprite(1000, 20, 50, 70, r'images\keys.png')
-exit = Game_sprite(5, 600, 100, 50, r'images\exit.png')
-exit_2 = Game_sprite(400, 430, 100, 50, r'images\exit.png')
+
 
 bullets = pygame.sprite.Group()
 bullets_enemy = pygame.sprite.Group()
 
 
 walls = pygame.sprite.Group()
-wall1 = Game_sprite(85, 6, 5, 200, r'images\wol.jpg') 
-walls.add(wall1)
-wall2 = Game_sprite(85, 206, 250, 5, r'images\wol.jpg')
-walls.add(wall2)
-wall3 = Game_sprite(335, 206, 5, 200, r'images\wol.jpg')
-walls.add(wall3)
-wall4 = Game_sprite(600, 300, 200, 5, r'images\wol.jpg')
-walls.add(wall4)
-wall5 = Game_sprite(5, 565, 1000, 5, r'images\wol.jpg')
-walls.add(wall5)
-wall6 = Game_sprite(150, 570, 5, 250, r'images\wol.jpg')
-walls.add(wall6)
-wall7 = Game_sprite(800, 306, 5, 250, r'images\wol.jpg')
-walls.add(wall7)
-wall8 = Game_sprite(220, 210, 5, 190, r'images\wol.jpg')
-walls.add(wall8)
-wall9 = Game_sprite(950, 200, 250, 5, r'images\wol.jpg')
-walls.add(wall9)
-#wall10 = Game_sprite(950, 0, 5, 140, r'images\wol.jpg')
-#walls.add(wall10)
-wall11 = Game_sprite(600, 300, 5, 250, r'images\wol.jpg')
-walls.add(wall11)
-wall12 = Game_sprite(2500, 6, 5, 100, r'images\wol.jpg')
-walls.add(wall12)
-wall13 = Game_sprite(650, 0, 5, 150, r'images\wol.jpg')
-walls.add(wall13)
-wall14 = Game_sprite(800, 150, 5, 150, r'images\wol.jpg')
-walls.add(wall14)
-wall15 = Game_sprite(800, 320, 200, 5, r'images\wol.jpg')
-walls.add(wall15)
-wall16 = Game_sprite(1000, 450, 200, 5, r'images\wol.jpg')
-walls.add(wall16)
+
 
 btn_1 = Buton(550, 350, 70, 50, 'lvl 1', 30, LVL_1, LVL_1_CLICK, TEXT, 10, 10)
 btn_2 = Buton(550, 420, 70, 50, 'lvl 2', 30, LVL_2, LVL_2_CLICK, TEXT, 10, 10)
 btn_3 = Buton(550, 490, 70, 50, 'lvl 3', 30, LVL_3, LVL_3_CLICK, TEXT, 10, 10)
 btn_exit = Buton(550, 560, 70, 50, 'exit', 30, EXIT, LVL_3_CLICK, TEXT, 10, 10)
 
+def create_lvl_1():
+    global player, frukt, key, bonus, exit, wall6
+    player = Player(5, 6, 50, 70, r'images\Luffy.png')
+    frukt = Game_sprite(90, 140, 40, 60, r'images\frukt.png')
+    bonus = Game_sprite(250, 250, 50, 70, r'images\bonus.png')
+    key = Game_sprite(1000, 20, 50, 70, r'images\keys.png')
+    exit = Game_sprite(5, 600, 100, 50, r'images\exit.png')
 
-def create_lvl_2():
+
     fon = pygame.image.load(fila_path(r"images\turma.jpg"))
     fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
+    
+    pygame.mixer.music.load(fila_path(r'music\lvl1_music.mp3'))
+    pygame.mixer.music.set_volume(0.2)
+    pygame.mixer.music.play(-1)
 
     bullets.empty()
     bullets_enemy.empty()
+
+    enemys.empty()
+    enemy = Enemy(600, 480, 50, 70, r'images\prison.png', 0, 600, 'left', 4)
+    enemys.add(enemy)
+    enemy2 = Enemy(150, 140, 50, 70, r'images\Crocodile.png', 150, 680, 'right', 5)
+    enemys.add(enemy2)
+    enemy3 = Shot(1150, 110, 50, 90, r'images\katakuri.png', 0, 200, 'up', 5, 50)
+    enemys.add(enemy3)
+    enemy4 = Enemy(1150, 630, 50, 70, r'images\leopard.png', 155, 1150, 'left', 7)
+    enemys.add(enemy4)
+
+    walls.empty()
+    wall1 = Game_sprite(85, 6, 5, 200, r'images\wol.jpg') 
+    walls.add(wall1)
+    wall2 = Game_sprite(85, 206, 250, 5, r'images\wol.jpg')
+    walls.add(wall2)
+    wall3 = Game_sprite(335, 206, 5, 200, r'images\wol.jpg')
+    walls.add(wall3)
+    wall4 = Game_sprite(600, 300, 200, 5, r'images\wol.jpg')
+    walls.add(wall4)
+    wall5 = Game_sprite(5, 565, 1000, 5, r'images\wol.jpg')
+    walls.add(wall5)
+    wall6 = Game_sprite(150, 570, 5, 250, r'images\wol.jpg')
+    walls.add(wall6)
+    wall7 = Game_sprite(800, 306, 5, 250, r'images\wol.jpg')
+    walls.add(wall7)
+    wall8 = Game_sprite(220, 210, 5, 190, r'images\wol.jpg')
+    walls.add(wall8)
+    wall9 = Game_sprite(950, 200, 250, 5, r'images\wol.jpg')
+    walls.add(wall9)
+    wall11 = Game_sprite(600, 300, 5, 250, r'images\wol.jpg')
+    walls.add(wall11)
+    wall12 = Game_sprite(2500, 6, 5, 100, r'images\wol.jpg')
+    walls.add(wall12)
+    wall13 = Game_sprite(650, 0, 5, 150, r'images\wol.jpg')
+    walls.add(wall13)
+    wall14 = Game_sprite(800, 150, 5, 150, r'images\wol.jpg')
+    walls.add(wall14)
+    wall15 = Game_sprite(800, 320, 200, 5, r'images\wol.jpg')
+    walls.add(wall15)
+    wall16 = Game_sprite(1000, 450, 200, 5, r'images\wol.jpg')
+    walls.add(wall16)
+
+
+
+    
+
+
+def create_lvl_2():
+    global player, exit_2, frukt2
+    exit_2 = Game_sprite(400, 430, 100, 50, r'images\exit.png')
+    frukt2 = Game_sprite(90, 240, 40, 60, r"images\Mochi.png")
+    player = Player(5, 6, 50, 70, r'images\Luffy.png')
+
+    fon = pygame.image.load(fila_path(r"images\turma.jpg"))
+    fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
+
+    pygame.mixer.music.load(fila_path(r'music\fon_music_lvl2.mp3'))
+    pygame.mixer.music.set_volume(0.2)
+    pygame.mixer.music.play(-1)
+
+    bullets.empty()
+    bullets_enemy.empty()
+
     enemys.empty()
     #! Створи ворогів діма!
-    enemy_2 = Enemy(600, 480, 50, 70, r'images\prison.png', 0, 500, 'left', 4)
+    enemy_2 = Enemy(600, 420, 50, 70, r'images\prison.png', 0, 500, 'left', 4)
     enemys.add(enemy_2)
-    enemy2_2 = Enemy(600, 480, 50, 70, r'images\Crocodile.png', 0, 600, 'left', 4)
+    enemy2_2 = Enemy(600, 450, 50, 70, r'images\Crocodile.png', 0, 600, 'left', 4)
     enemys.add(enemy2_2)
+
     walls.empty()
     #! створити стіни!
     wall1_2 = Game_sprite(1000, 0, 5, 200, r'images\wol.jpg')
     walls.add(wall1_2)
     wall2_2 = Game_sprite(150, 300, 350, 5, r'images\wol.jpg')
     walls.add(wall2_2)
-    wall3_2 = Game_sprite(500, 300, 200, 5, r'images\wol.jpg')
+    wall3_2 = Game_sprite(500, 300, 5, 200, r'images\wol.jpg')
     walls.add(wall3_2)
 
-    player.rect.x = 5
-    player.rect.y = 66
+def create_lvl_3():
+    pass
+    
 
 
 
 
-lvl = 2
-create_lvl_2()
+lvl = 0
 game = True
 
 while game == True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print(event.pos)
         if lvl == 1:
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RIGHT or event.key == pygame.K_d:
@@ -347,9 +380,36 @@ while game == True:
                 if event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     player.speedy = 0
 
+        elif lvl == 0:
+            if event.type == pygame.MOUSEMOTION:
+                x, y = event.pos
+                if btn_1.rect.collidepoint(x, y):
+                    btn_1.color = btn_1.click
+                elif btn_2.rect.collidepoint(x, y):
+                    btn_2.color = btn_2.click
+                elif btn_3.rect.collidepoint(x, y):
+                    btn_3.color = btn_3.click
+                elif btn_exit.rect.collidepoint(x, y):
+                    btn_exit.color = btn_exit.click
+                else:
+                    btn_1.color = btn_1.no_click
+                    btn_2.color = btn_2.no_click
+                    btn_3.color = btn_3.no_click
+                    btn_exit.color = btn_exit.no_click
 
-
-
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                x, y = event.pos
+                if btn_1.rect.collidepoint(x, y):
+                    create_lvl_1()
+                    lvl = 1
+                elif btn_2.rect.collidepoint(x, y):
+                    create_lvl_2()
+                    lvl = 2
+                elif btn_3.rect.collidepoint(x, y):
+                    create_lvl_3()
+                    lvl = 3
+                elif btn_exit.rect.collidepoint(x, y):
+                    game = False
 
 
     if lvl == 1:
